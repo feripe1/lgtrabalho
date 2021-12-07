97d91a9e678bfdada7754b492eebe963e323361c
feripe1 <89111098+feripe1@users.noreply.github.com>
2021-12-06 22:17:56 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -3,20 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <windows.h>
 
 #define MAX 20
 #define MAXNPEDIDOS 199
 
 int nmPedido;
 int i = 0;
-
-struct produto
-{
-    int codProd;
-    char descrProd[20];
-    float custoProd;
-    bool demora;
-} cardapio[MAX];
+int cont = 0;
+bool sair;
 
 struct pedido
 {
@@ -48,16 +43,14 @@ struct fila
 int main()
 {
     void importNPedido(void);
-    void importPagamentos(void);
-    void importCardapio(void);
+    void verFila(void);
     void importFila(void);
-    void verPagamentos(void);
+    void atualizarPosicaoFila(void);
 
     importNPedido();
-    importPagamentos();
-    importCardapio();
     importFila();
-    verPagamentos();
+    atualizarPosicaoFila();
+    verFila();
 
     getch();
     return 0;
@@ -81,133 +74,149 @@ void importNPedido(void)
     fclose(Arq);
 }
 
-void importCardapio(void)
-{
-    system("cls");
-
-    FILE *Arq;
-    Arq = fopen("PRODUTOS.DAT", "r+b");
-    if (Arq == NULL)
-    {
-        printf("\nErro ao abrir PRODUTOS.DAT");
-        getch();
-        exit(0);
-    }
-    while (!feof(Arq))
-    {
-        fread(&cardapio, sizeof(cardapio), 1, Arq);
-    }
-    fclose(Arq);
-}
-
-void importPedidos(void) {
-    system("cls");
-
-    FILE *Arq;
-    Arq = fopen("PEDIDOS.DAT", "r");
-    if (Arq == NULL)
-    {
-        printf("\nErro ao abrir PRODUTOS.DAT");
-        getch();
-    }
-    while (!feof(Arq))
-    {
-        fread(&pedido, sizeof(pedido), 1, Arq);
-    }
-
-    fclose(Arq);
-}
-
-
-void importPagamentos(void)
-{
-    system("cls");
-
-    FILE *Arq;
-    Arq = fopen("PAGAMENTOS.DAT", "r");
-    if (Arq == NULL)
-    {
-        printf("\nErro ao abrir PRODUTOS.DAT");
-        getch();
-    }
-    while (!feof(Arq))
-    {
-        fread(&pagamento, sizeof(pagamento), 1, Arq);
-    }
-
-    fclose(Arq);
-}
-
 void importFila(void) {
-    int cont = 1;
-
     FILE *Arq;
     Arq = fopen("FILA.DAT", "rb");
     fread(&fila, sizeof(fila), 1, Arq);
 
     for (i = 0; i <= nmPedido; i++) {
         for ( int j = 0; j <= nmPedido; j++) {
-            if (i != fila[i].pagamento.nmPedido) {
+            if (i != fila[i].pagamento.nmPedido ) {
                 aux = fila[fila[i].pagamento.nmPedido];
                 fila[fila[i].pagamento.nmPedido] = fila[i];
                 fila[i] = aux;
             }
         }
     }
-
-    for (i = 0; i <= nmPedido; i++) {
-        if (fila[i].pagamento.nmPedido != 0) {
-            fila[i].posicaoFila = cont;
-            cont++;
-        }
-    }
     fclose(Arq);
 }
 
-void verPagamentos(void) {
-    void verdetalhes(int nmpedido);
+void atualizarPosicaoFila(void) {
+    cont = 0;
+    for (i = 0; i <= nmPedido; i++)
+    {
+        if (fila[i].pagamento.nmPedido != 0 && fila[i].pagamento.entregue == false)
+        {
+            cont++;
+            fila[i].posicaoFila = cont;
+        }
+    }
+}
+
+void verFila(void) {
+    void verDetalhes(int nmpedido);
+    void importFila(void);
     system("cls");
     int i, j;
     int opc;
-    printf("--------------------PEDIDOS NA FILA-------------------\n");
-    printf("     NM DO PEDIDO      NOME        POSICAO NA FILA\n");
-    for (i = 0; i <= nmPedido; i++) {        
-        if (fila[i].pagamento.entregue == false && fila[i].pagamento.nmPedido != 0)
-        {
-            printf("%10d %18s %16d\n", fila[i].pagamento.nmPedido, fila[i].pagamento.nomePessoa, fila[i].posicaoFila);
-        }
-            
-    }
-    printf("------------------------------------------------------\n");
-    printf("\nDigite o número do pedido para ver mais detalhes: ");
-    scanf("%d", &opc);
-    verdetalhes(opc);
     
-}
-
-void verdetalhes(int nmpedido){
-    if (fila[nmpedido].pagamento.total == 0) {
-        printf("Número inválido.");
-    }
-
-    else {
-        printf("\n\n----------------Lançamentos do pedido----------------\n");
-        printf("     COD  NOME DO PRODUTO    QUANTIDADE    SUBTOTAL\n");
-        for (i = 0; i < 20; i++)
+    do {
+        if (cont > 0)
         {
-            if (fila[nmpedido].pedido[i].qntdProd > 0)
+            system("cls");
+            printf("--------------------PEDIDOS NA FILA-------------------\n");
+            printf("     NM DO PEDIDO        NOME         POSICAO NA FILA\n");
+            for (i = 0; i <= nmPedido; i++)
             {
-                printf("%7d %14s %12d %13.2f\n", fila[nmpedido].pedido[i].codProd, fila[nmpedido].pedido[i].descrProd,
-                                                fila[nmpedido].pedido[i].qntdProd, fila[nmpedido].pedido[i].subtotal);
+                if (fila[i].pagamento.entregue == false && fila[i].pagamento.nmPedido != 0)
+                {
+                    printf("%10d %18s %16d\n", fila[i].pagamento.nmPedido, fila[i].pagamento.nomePessoa, fila[i].posicaoFila);
+                }
+            }
+            printf("------------------------------------------------------\n");
+            printf("\nDigite o número do pedido para ver mais detalhes: ");
+            scanf("%d", &opc);
+
+            if (opc == 0)
+            {
+                system("cls");
+                printf("Fechando o sistema");
+                getch();
+                sair = true;
+            }
+
+            else if (fila[opc].pagamento.total == 0)
+            {
+                system("cls");
+                printf("Número inválido.\n");
+                printf("...Aperte qualquer tecla para continuar...");
+                getch();
+            }
+            else
+            {
+                verDetalhes(opc);
             }
         }
-        printf("------------------------------------------------------\n");
-        printf("                                     Total: %.2f\n", fila[nmpedido].pagamento.total);
-        printf("               Tempo de preparo total: %d minutos\n", fila[nmpedido].pagamento.demoraTotal);
-        printf("------------------------------------------------------");
+        else {
+            system("cls");
+            printf("Não há pedidos na fila");
+            sair = true;
+        }
+    } while (sair == false);
+}
+
+void verDetalhes(int x){
+    char opc;
+    void prepararPedido(int x);
+    void verFila(void);
+
+    printf("\n\n----------------Lançamentos do pedido----------------\n");
+    printf("     COD  NOME DO PRODUTO    QUANTIDADE    SUBTOTAL\n");
+    for (i = 0; i < 20; i++)
+    {
+        if (fila[x].pedido[i].qntdProd > 0)
+        {
+            printf("%7d %14s %12d %13.2f\n", fila[x].pedido[i].codProd, fila[x].pedido[i].descrProd,
+                   fila[x].pedido[i].qntdProd, fila[x].pedido[i].subtotal);
+        }
+    }
+    printf("------------------------------------------------------\n");
+    printf("                                     Total: %.2f\n", fila[x].pagamento.total);
+    printf("            Tempo de preparo previsto: %d minutos\n", fila[x].pagamento.demoraTotal);
+    printf("------------------------------------------------------\n");
+    printf("Deseja realizar a entrega? ( 0- Não || 1- Sim )\n");
+
+    opc = getch();
+    switch (opc)
+    {
+    case '0':
+        break;
+    case '1':
+        prepararPedido(x);
+        break;
+    default:
+        break;
     }
 }
 
-void prepararPedido(void) {
+void prepararPedido(int x) {
+    void exportFila(void);
+    void atualizarPosicaoFila(void);
+    if (fila[x].posicaoFila == 1) {
+        fila[x].pagamento.entregue = true;
+        printf("Preparando o pedido\n");
+        Sleep(10000);
+        printf("Pedido número %d entregue com sucesso.\n", x);
+        exportFila();
+        atualizarPosicaoFila();
+        getch();
+    }
+    else {
+        printf("\nO pedido número %d não é o primeiro na lista de espera.", x);
+        getch();
+    }
+}
 
+void exportFila(void) {
+    FILE *Arq;
+    Arq = fopen("FILA.DAT", "wb");
+
+    for (i = 0; i <= nmPedido; i++)
+    {
+        if (fila[i].pagamento.entregue == false && fila[i].pagamento.total != 0)
+        {
+            fwrite(&fila[i], sizeof(fila[i]), 1, Arq);
+        }
+    }
+    fclose(Arq);
 }
