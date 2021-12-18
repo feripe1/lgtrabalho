67bff132f6c5154f4c7ad8564286d1f16b0e6ddf
feripe1 <89111098+feripe1@users.noreply.github.com>
2021-12-17 21:16:03 -0300

    Update administracao.c

--- administracao.c
+++ administracao.c
@@ -14,6 +14,7 @@ struct produto
     char descrProd[20];
     float custoProd;
     int demoraMinutos;
+    int tipoProduto;
 } cardapio[MAX];
 
 struct pagamento
@@ -48,30 +49,36 @@ void opcoes(void) {
     void verPagamentos(void);
     void retirarItem(void);
     void verCartoes(void);
-
-    printf("Selecione uma opcao: \n");
-    printf("(1) Ver os últimos pagamentos\n(2) Ver cartões\n(3) Adicionar item ao cardapio\n(4) Retirar item");
+    
+    do {
+    system("mode 45, 33");
+    system("color f0");
+    printf("\n Selecione uma opcao: \n");
+    printf(" 1 Ver os últimos pagamentos\n 2 Ver cartões\n 3 Adicionar item ao cardapio\n 4 Retirar item\n ESQ = Sair\n");
+    
     opc = getch();
-
     switch (opc) {
-    case '1':
-        verPagamentos();
-        break;
-    case '2':
-        verCartoes();
-        break;
-    case '3':
-        adicionarItem();
-        break;
-
-    case '4':
-        retirarItem();
-        break;
-    default:
-        printf("Opção inválida");
-        getch();
-        break;
-    }
+	    case '1':
+	        verPagamentos();
+	        break;
+	    case '2':
+	        verCartoes();
+	        break;
+	    case '3':
+	        adicionarItem();
+	        break;
+	    case '4':
+	        retirarItem();
+	        break;
+	    case 27:
+	    	exit(0);
+	    	break;
+	    default:
+	        printf("Opção inválida");
+	        getch();
+	        break;
+	    }
+	} while(opc != 27);
 }
 
 void importCardapio(void) {
@@ -129,12 +136,15 @@ void importPagamentos(void) {
 void verPagamentos(void) {
     system("cls");
     int i;
+    printf("\n---------------------------------------------\n");
+    printf(" COD       FORMA DE PAGAMENTO       TOTAL\n");
+    printf("---------------------------------------------\n");
 
     for (i = 0; i < 199; i++)
     {
         if (pagamento[i].total >0)
         {
-            printf("%1d %2s %2.2f \n", pagamento[i].nmPedido,  pagamento[i].formaPagamento, pagamento[i].total);
+            printf("% 3d %19s %17.2f \n", pagamento[i].nmPedido,  pagamento[i].formaPagamento, pagamento[i].total);
         }
         
     }
@@ -145,27 +155,32 @@ void verCartoes(void)
 {
     system("cls");
     int i;
+    printf("\n---------------------------------------------\n");
+    printf("       NMPEDIDO           CARTAO\n");
+    printf("---------------------------------------------\n");
 
     for (i = 0; i < 199; i++)
     {
         if (pagamento[i].total > 0 && pagamento[i].nmCartao[i] != '\0')
         {
-            printf("%1d %2s \n", pagamento[i].nmPedido, pagamento[i].nmCartao);
+            printf("%10d %25s \n", pagamento[i].nmPedido, pagamento[i].nmCartao);
         }
     }
     getch();
 }
 
 void adicionarItem(void) {
-    char opc;
+	void limparItemRemovido(int cod);
+    char opcContinuar, opcConfirmar;
     int cod, aux;
     void verCardapio(void);
+    bool voltar = false;
     FILE *fp;
 
-    fp = fopen("PRODUTOS.DAT", "w");
+    fp = fopen("PRODUTOS.DAT", "w+b");
 
     if (fp == NULL) {
-        printf("Error opening file\n");
+        printf("Erro ao tentar abrir PRODUTOS.DAT\n");
         
     }
 
@@ -173,60 +188,85 @@ void adicionarItem(void) {
         fflush(stdin);
         system("cls");
         verCardapio();
-        printf("cod ");
+        printf(" cod ( 0 = Voltar ) ");
         scanf("%d", &cod);
-
-        cardapio[cod].codProd = cod;
-
-        printf("nome ");
-        fflush(stdin);
-        gets(cardapio[cod].descrProd);
-
-        printf("preco ");
-        fflush(stdin);
-        scanf("%f", &cardapio[cod].custoProd);
-
-        printf("Demora? ");
-        scanf("%d", &cardapio[cod].demoraMinutos);
-        
-        printf("Deseja continuar? esq para não");
-        opc = getch();
-        fwrite(&cardapio, sizeof(cardapio), 1, fp);
-    } while (opc != 27);
-    
+        if (cod == 0){
+        	voltar = true;
+		}
+		else {
+			cardapio[cod].codProd = cod;
+	        printf(" nome ");
+	        fflush(stdin);
+	        gets(cardapio[cod].descrProd);
+	
+	        printf(" preco ");
+	        fflush(stdin);
+	        scanf("%f", &cardapio[cod].custoProd);
+	
+	        printf(" Demora? ");
+	        scanf("%d", &cardapio[cod].demoraMinutos);
+	
+	        printf(" Tipo de produto\n 1 = Lanche\n 2 = Liquido em lata\n 3 = Liquido com preparo\n 4 = Acompanhamento\n ");
+	        scanf("%d", &cardapio[cod].tipoProduto);
+	        
+	        printf("\n Confirmar adi��o ao card�pio? ESQ = N�o)");
+	        printf("\n ");
+	        opcConfirmar = getch();
+	        
+	        if(opcConfirmar == 27) {
+	        	limparItemRemovido(cod);
+	        	voltar = true;
+			}
+			else {
+				printf("\n\n Deseja continuar? ESQ = N�o");
+	        	opcContinuar = getch();
+			}
+		}
+    } while (opcContinuar != 27 && voltar == false);
+    fwrite(&cardapio, sizeof(cardapio), 1, fp);
     fclose(fp);
 }
 
+void limparItemRemovido(int cod){
+	int i;
+	cardapio[cod].codProd = '\0';
+	for(i = 0; i <20; i++)
+    	cardapio[cod].descrProd[i] = '\0';
+    	
+    cardapio[cod].custoProd = '\0';
+    cardapio[cod].demoraMinutos = '\0';
+    cardapio[cod].tipoProduto = '\0';
+}
+
 void retirarItem(void) {
+	void limparItemRemovido(int cod);
     void verCardapio(void);
     int cod, i;
-    system("cls");
-    verCardapio();
-    printf("Qual o código do item que deseja retirar?");
-    scanf("%d", &cod);
-
-    cardapio[cod].codProd = 0;
-    cardapio[cod].custoProd = 0;
-
+    char opc;
     FILE *fp;
 
     fp = fopen("PRODUTOS.DAT", "w+b");
 
     if (fp == NULL)
     {
-        printf("Error opening file\n");
+        printf("Erro ao tentar abrir PRODUTOS.DAT\n");
         
     }
+    do {
+    system("cls");
+    verCardapio();
+    printf(" Qual o código do item que deseja retirar?\n\n");
+    scanf("%d", &cod);
 
-    for (i = 0; i < MAX; i++)
-    {
-        if (cardapio[i].custoProd != 0)
-        {
-            fwrite(&cardapio, sizeof(cardapio), 1, fp);
-            fclose(fp);
-        }
-    }
+    limparItemRemovido(cod);
     
+    printf(" Deseja continuar? ESQ = N�o\n ");
+    opc = getch();
+	} while(opc != 27);
+
+    fwrite(&cardapio, sizeof(cardapio), 1, fp);
+    fclose(fp);
+
     getch();
 
 }
